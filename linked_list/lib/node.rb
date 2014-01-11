d05cff8740c7f8773e808e38596ca98d4d633e29
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-11 14:48:25 -0700

    linked list files

---
+++ linked_list/lib/node.rb
@@ -0,0 +1,17 @@
+class Node
+  attr_reader :data
+  #attr_accessor :link
+  attr_accesor :link
+
+  def initialize(data)
+    @data = data
+  end
+
+  def link
+    @link
+  end
+
+  def link=(link)
+    @link = link
+  end
+end
---
+++ linked_list/linked_list.rb
@@ -0,0 +1,52 @@
+require './lib/node'
+class LinkedList
+  attr_reader :head
+
+  def count
+    current_count = 0
+    if head
+      current = head
+      previous = nil
+      current_count += 1 
+      while current.link 
+        previous = current 
+        current = current.link 
+        current_count += 1 
+      end
+    end
+    return current_count
+  end
+
+  def push(data)
+    new_node = Node.new(data)
+    return @head = new_node if @head.nil?
+      current = head
+      previous = nil 
+      while current.link 
+        previous = current 
+        current = current.link   
+      end
+      current.link = new_node
+  end
+
+  def pop
+    current = head
+      previous = nil 
+      while current.link 
+        previous = current 
+        current = current.link   
+      end
+      previous.link = nil
+  end
+
+  def shift
+    current = head.link 
+    @head = current 
+  end
+end
+
+
+
+
+  
+
