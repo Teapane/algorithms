12168565d967ae52359ca57d227efb7bc662c9ad
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-11 22:58:21 -0700

    Update node_test.rb

--- linked_list/test/node_test.rb
+++ linked_list/test/node_test.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/node'
 
-class NodeTest < Minitest::Test 
+class NodeTest < Minitest::Test  
 
   def test_it_exists
     assert Node 
