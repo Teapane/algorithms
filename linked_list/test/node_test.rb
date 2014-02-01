ce86420b54e60ad3d1e3d47a6ae7ee20fe755409
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-31 22:43:21 -0700

    Update node_test.rb

--- linked_list/test/node_test.rb
+++ linked_list/test/node_test.rb
@@ -10,13 +10,13 @@ class NodeTest < Minitest::Test
   end
 
   def test_it_has_data
-    node = Node.new("Horse")
-    assert_equal "Horse", node.data
+    node = Node.new("valid")
+    assert_equal "Hor", node.data
   end
 
   def test_it_has_a_link
-    node = Node.new("Horse")
-    node.link = Node.new("Donkey")
-    assert_equal "Donkey", node.link.data
+    node = Node.new("valid")
+    node.link = Node.new("valids best friend")
+    assert_equal "valids best frien", node.link.data
   end
 end
